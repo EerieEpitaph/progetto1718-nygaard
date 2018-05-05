4267f4b44c91905b87cb7d43d021d715c261ebf3
phonizx <phinkie@GiovanDonato.Devp>
2018-05-05 19:40:24 +0200

    Graph per la gestione dei Mention e aggiunta Model
    Aggiunta di un model per la gestione dei dati dell'applicativo, in questo Model ci saranno tutte le classi che si occuperanno
    della gestione dei dati dell'applicativo, tra cui la rete sociale.
    Aggiunto il grafo per i  mention e la funzione parseMessages che si occupa di prendere tutti i messaggi di un workspace e
    individuare l'utente autore del @mention e l'utente menzionato creando un arco tra i due soggetti.

---
+++ src/main/java/it/uniba/model/MentionGraph.java
@@ -0,0 +1,72 @@
+package it.uniba.model;
+import it.uniba.workdata.Message;
+import it.uniba.workdata.User;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.google.common.graph.MutableValueGraph;
+import com.google.common.graph.ValueGraphBuilder;
+
+
+public class MentionGraph {
+	private MutableValueGraph<User, Integer>  snagraph = ValueGraphBuilder.directed().build();
+	
+	public MentionGraph(ArrayList<Message> message,HashMap<String, User> users )
+	{
+		parseMessage(message,users);
+	}
+	
+	void parseMessage(ArrayList<Message> message,HashMap<String, User> users )
+	{
+		Pattern pattern = Pattern.compile("\\<@.*?\\>");
+		//Matcher matcher;
+		
+		for(Message msg : message)
+		{
+			/* controlli esistenza dei nodi prima di inserirli */ 
+			if(msg.getText().contains("<@"))
+			{
+				User utenteu = users.get(msg.getUser());
+				if(!snagraph.nodes().contains(utenteu))
+						snagraph.addNode(utenteu);
+				
+				Matcher matcher = pattern.matcher(msg.getText());
+				
+				if(matcher.find())
+				{
+					while (matcher.find()) 
+					{
+						String filterstring  = matcher.group(0).replaceAll("<@", " ").replaceAll(">", "");
+						User utentev = users.get(filterstring);
+						if(!snagraph.nodes().contains(utentev))
+							snagraph.addNode(utentev);
+						/* controllo se esiste gia un arco tra i due utenti: Se esiste aggiungo 
+							 +1 al mention altrimenti se non esiste creo l'arco */
+						if(!snagraph.hasEdgeConnecting(utenteu, utentev))
+							snagraph.putEdgeValue(utenteu, utentev, 0);
+						else
+						{
+							int mentioncount = snagraph.edgeValue(utenteu, utentev).get() + 1;
+							// cercare un modifica  del peso dell'arco tra due nodi 
+							snagraph.removeEdge(utenteu, utentev);
+							snagraph.putEdgeValue(utenteu, utentev, mentioncount);
+						}
+					}		
+ 				}
+				
+					
+			}	
+		}
+	}
+	
+	public MutableValueGraph<User, Integer>  getGraph()
+	{
+		return snagraph;
+	}
+	
+	
+	
+}
