82cba0fa82260d94b82abccda43c59f65f19bfd9
EerieEpitaph <o.lischio@studenti.uniba.it>
2018-05-19 13:07:51 +0200

    Interface remodeling

--- src/main/java/it/uniba/controller/input/CommandInterpreter.java
+++ src/main/java/it/uniba/controller/input/CommandInterpreter.java
@@ -9,153 +9,75 @@ import it.uniba.controller.input.CommandParser;
 import it.uniba.controller.input.CommandParser.*;
 
 // da prendere datacontroller 
-public class CommandInterpreter {
+public class CommandInterpreter
+{
 
-	public void executeCommands(CommandParser parser, DataController dataCtr) throws ZipException, IOException {
-		CommBaseArgs baseArgs = parser.getBaseArgs();
-		CommWorkspace workspace = parser.getCommWorkspace();
+    public void executeCommands(CommandParser parser, DataController dataCtr)
+            throws ZipException, IOException
+    {
+        CommandParserInterface bridge = parser;
 
-		// Argomenti singoli immessi
-		if (baseArgs.isActive()) {
-			// Ho chiesto help
-			if (baseArgs.getHelpStatus())
-				showHelp();
-		}
+        if (parser.help())
+            showHelp();
 
-		// -w nomeWorkspace inserito
-		else if (workspace.isActive()) {
-			// nomeWorkspace valido
-			if (workspace.isValidWorkspace()) {
-				String workspaceName = workspace.getWorkspaceName();
-				// fileParser.load(workspaceName); // load workspace
-				dataCtr.updateModel(workspaceName);
-				// Fileparser ha caricato qualcosa
-				if (dataCtr.hasLoaded()) {
-					// -u inserito
-					if (workspace.getMembersStatus())
-						dataCtr.printMembers();
+        else if (parser.users())
+            dataCtr.printMembers();
 
-					// -c inserito
-					else if (workspace.getChannelsStatus())
-						dataCtr.printChannels();
+        else if (parser.channels())
+            dataCtr.printChannels();
 
-					// -cu inserito
-					else if (workspace.getExtChannelsStatus())
-						dataCtr.printMembers4Channel();
+        else if (parser.extendedChannels())
+            dataCtr.printMembers4Channel();
 
-					// -uc inserito
-					else if (workspace.isValidFilter())
-						dataCtr.printChannelMembers(workspace.getChannelFilter());
+        else if (parser.userInChannel())
+            dataCtr.printChannelMembers(parser.getChannelFilter());
 
-					// -m riconosciuto
-					else if (workspace.getMentionParams() != null) { // issue#37
-																		// -m
-						if (workspace.getMentionParams().length == 0) {
-							// Stampa tutti i mention del workspace
-							dataCtr.printMention("");
-						}
-						// -m in x
-						else if (wantsIn(workspace.getMentionParams())) {
-							String inChannel = workspace.getMentionParams()[1];
-							// Stampa tutti i mention in un channel x
-							if (!inChannel.equals(""))
-								dataCtr.printMention(inChannel);
-						}
+        else if (parser.mentions())
+        {
+            Boolean weight = false;
+            String in = "";
+            String name = "";
 
-						// issue#38
-						// -m from x
-						else if (wantsFrom(workspace.getMentionParams())) {
-							String fromWho = workspace.getMentionParams()[1];
-							// Stampa tutti i mention effettuati da x
-							dataCtr.printMentionsFromUser(fromWho, "");
-						}
-						// -m from x in y
-						else if (wantsFromIn(workspace.getMentionParams())) {
-							String fromWho = workspace.getMentionParams()[1];
-							String inChannel = workspace.getMentionParams()[3];
-							if (!inChannel.equals(""))
-								dataCtr.printMentionsFromUser(fromWho, inChannel);
-							// Stampa tutti i mention effettuati da x nel channel y
+            if (parser.weighted())
+                weight = true;
+            if (parser.in())
+                in = parser.getInWhat();
 
-						}
-						// issue#39
-						// -m to x
-						else if (wantsTo(workspace.getMentionParams())) {
-							String toWho = workspace.getMentionParams()[1];
-							dataCtr.printMentionsToUser(toWho, "");
-							// Stampa tutti i mention in cui viene menzionato x
-						}
+            if (parser.from())
+            {
+                name = parser.getFromWho();
+                // Qualcosa tipo printMentionsFrom(name, in, weight); TODO
+            } else if (parser.to())
+            {
+                name = parser.getToWho();
+                // Qualcosa tipo printMentionsTo(name, in, weight); TODO
+            } else
+            {
+                // Qualcosa tipo printMentions(name, in, weight); TODO
+            }
+        } else
+            throw new IllegalStateException();
+    }
 
-						// -m to x in y
-						else if (wantsToIn(workspace.getMentionParams())) {
-							String toWho = workspace.getMentionParams()[1];
-							String inChannel = workspace.getMentionParams()[3];
-							dataCtr.printMentionsToUser(toWho, inChannel);
-							// Stampa tutti i mention in cui � menzionato x nel channel y
-						} else
-							throw new IllegalStateException();
-					} else
-						throw new IllegalStateException();
-				} else
-					throw new IllegalStateException();
-			} else
-				throw new IllegalStateException();
-		} else
-			throw new IllegalStateException();
-	}
-
-	public void showHelp() {
-		System.out.println("Usage:");
-		System.out.println("help - shows this help\n");
-		System.out.println(
-				"-w \"path\\to\\file.zip\" ( -c | -u | -uc <channelFilter> | -cu | -m [from <x>] | [to <y>] [in <z>])");
-		System.out.println("\t-w parses a workspace");
-		System.out.println("\t-c prints all the channels in the specified workspace");
-		System.out.println("\t-u prints all the users in the specified workspace");
-		System.out.println("\t-uc <channelFilter> prints all the users in the specified channel ");
-		System.out.println("\t-cu prints all the channels with their users\n");
-		System.out.println("\t-m prints all the mentions in a workspace");
-		System.out.println("\t\t-m from <user> filters the mentioner");
-		System.out.println("\t\t-m to <user> filters the mentioned");
-		System.out.println("\t\t-m in filters the channel");
-		System.out.println(
-				"\t\t-m from <user> OR to <user> in <channel> filters the mentioner or mentioned in the channel");
-	}
-
-	private Boolean wantsFrom(String[] mentionParams) {
-		if ((mentionParams.length == 2) && mentionParams[0].equals("from") && mentionParams[1] != null)
-			return true;
-		else
-			return false;
-	}
-
-	private Boolean wantsTo(String[] mentionParams) {
-		if ((mentionParams.length == 2) && mentionParams[0].equals("to") && mentionParams[1] != null)
-			return true;
-		else
-			return false;
-	}
-
-	private Boolean wantsIn(String[] mentionParams) {
-		if ((mentionParams.length == 2) && mentionParams[0].equals("in") && mentionParams[1] != null)
-			return true;
-		else
-			return false;
-	}
-
-	private Boolean wantsFromIn(String[] mentionParams) {
-		if ((mentionParams.length == 4) && mentionParams[0].equals("from") && mentionParams[1] != null
-				&& mentionParams[2].equals("in") && mentionParams[1] != null)
-			return true;
-		else
-			return false;
-	}
-
-	private Boolean wantsToIn(String[] mentionParams) {
-		if ((mentionParams.length == 4) && mentionParams[0].equals("to") && mentionParams[1] != null
-				&& mentionParams[2].equals("in") && mentionParams[1] != null)
-			return true;
-		else
-			return false;
-	}
+    public void showHelp()
+    {
+        System.out.println("Usage:");
+        System.out.println("help - shows this help\n");
+        System.out.println(
+                "-w \"path\\to\\file.zip\" ( -c | -u | -uc <channelFilter> | -cu | -m [from <x>] | [to <y>] [in <z>])");
+        System.out.println("\t-w parses a workspace");
+        System.out.println(
+                "\t-c prints all the channels in the specified workspace");
+        System.out.println(
+                "\t-u prints all the users in the specified workspace");
+        System.out.println(
+                "\t-uc <channelFilter> prints all the users in the specified channel ");
+        System.out.println("\t-cu prints all the channels with their users\n");
+        System.out.println("\t-m prints all the mentions in a workspace");
+        System.out.println("\t\t-m from <user> filters the mentioner");
+        System.out.println("\t\t-m to <user> filters the mentioned");
+        System.out.println("\t\t-m in filters the channel");
+        System.out.println(
+                "\t\t-m from <user> OR to <user> in <channel> filters the mentioner or mentioned in the channel");
+    }
 }
--- src/main/java/it/uniba/controller/input/CommandParser.java
+++ src/main/java/it/uniba/controller/input/CommandParser.java
@@ -9,172 +9,204 @@ import picocli.CommandLine;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-public class CommandParser implements CommandParserInterface {
-	// Comandi booleani senza parametri
-	public class CommBaseArgs {
-		private Boolean active = false;
+public class CommandParser implements CommandParserInterface
+{
+    // Comandi booleani senza parametri
+    public class CommBaseArgs
+    {
+        private Boolean active = false;
 
-		@Option(names = "help")
-		private boolean helpStatus;
+        @Option(names = "help")
+        private boolean helpStatus;
 
-		public Boolean isActive() {
-			return active;
-		}
+        public Boolean isActive()
+        {
+            return active;
+        }
 
-		public Boolean getHelpStatus() {
-			return helpStatus;
-		}
-	}
+        public Boolean getHelpStatus()
+        {
+            return helpStatus;
+        }
+    }
 
-	public class CommWorkspace {
-		private Boolean active = false;
+    public class CommWorkspace
+    {
+        private Boolean active = false;
 
-		@Parameters(index = "0", arity = "1")
-		String workspaceName;
+        @Parameters(index = "0", arity = "1")
+        String workspaceName;
 
-		@Option(names = "-u", arity = "0..1")
-		private boolean membersStatus;
+        @Option(names = "-u", arity = "0..1")
+        private boolean membersStatus;
 
-		@Option(names = "-c", arity = "0..1")
-		private boolean channelsStatus;
+        @Option(names = "-c", arity = "0..1")
+        private boolean channelsStatus;
 
-		@Option(names = "-cu", arity = "0..1")
-		private boolean extChannelsStatus;
+        @Option(names = "-cu", arity = "0..1")
+        private boolean extChannelsStatus;
 
-		@Option(names = "-uc", arity = "1")
-		private String channelFilter;
+        @Option(names = "-uc", arity = "1")
+        private String channelFilter;
 
-		@Option(names = "-m", arity = "0..4")
-		private String[] mentionParams;
+        @Option(names = "-m", arity = "0..4")
+        private String[] mentionParams;
 
-		public Boolean isActive() {
-			return active;
-		}
+        public Boolean isActive()
+        {
+            return active;
+        }
 
-		public String getWorkspaceName() {
-			return workspaceName;
-		}
+        public String getWorkspaceName()
+        {
+            return workspaceName;
+        }
 
-		public Boolean isValidWorkspace() {
-			return (workspaceName != null && workspaceName != "");
-		}
+        public Boolean isValidWorkspace()
+        {
+            return (workspaceName != null && workspaceName != "");
+        }
 
-		public Boolean getMembersStatus() {
-			return membersStatus;
-		}
+        public Boolean getMembersStatus()
+        {
+            return membersStatus;
+        }
 
-		public Boolean getChannelsStatus() {
-			return channelsStatus;
-		}
+        public Boolean getChannelsStatus()
+        {
+            return channelsStatus;
+        }
 
-		public Boolean getExtChannelsStatus() {
-			return extChannelsStatus;
-		}
+        public Boolean getExtChannelsStatus()
+        {
+            return extChannelsStatus;
+        }
 
-		public String getChannelFilter() {
-			return channelFilter;
-		}
+        public String getChannelFilter()
+        {
+            return channelFilter;
+        }
 
-		public Boolean isValidFilter() {
-			return (channelFilter != null && channelFilter != "");
-		}
+        public Boolean isValidFilter()
+        {
+            return (channelFilter != null && channelFilter != "");
+        }
 
-		public String[] getMentionParams() {
-			return mentionParams;
-		}
-	}
+        public String[] getMentionParams()
+        {
+            return mentionParams;
+        }
+    }
 
-	private CommBaseArgs baseArgs;
-	private CommWorkspace workspace;
+    private CommBaseArgs baseArgs;
+    private CommWorkspace workspace;
 
-	public CommandParser(String[] args) throws IllegalStateException {
-		baseArgs = new CommBaseArgs();
-		workspace = new CommWorkspace();
+    public CommandParser(String[] args) throws IllegalStateException
+    {
+        baseArgs = new CommBaseArgs();
+        workspace = new CommWorkspace();
 
-		CommandLine commandLine = new CommandLine(baseArgs).addSubcommand("-w", workspace);
+        CommandLine commandLine = new CommandLine(baseArgs).addSubcommand("-w",
+                workspace);
 
-		List<CommandLine> result = commandLine.parse(args);
+        List<CommandLine> result = commandLine.parse(args);
 
-		for (CommandLine x : result) {
-			// Gli "argomenti base" sarebbero sempre true, per com'� strutturata la
-			// libreria.
-			// In questo if setto la loro attivit� = true solo se, usando la riflessione,
-			// uno dei loro field � true
-			// Se pi� di un field e' true, throwo direttamente un'eccezione.
-			if (x.getCommand().getClass() == CommBaseArgs.class) {
-				baseArgs = (CommBaseArgs) x.getCommand();
+        for (CommandLine x : result)
+        {
+            // Gli "argomenti base" sarebbero sempre true, per com'�
+            // strutturata la
+            // libreria.
+            // In questo if setto la loro attivit� = true solo se, usando la
+            // riflessione,
+            // uno dei loro field � true
+            // Se pi� di un field e' true, throwo direttamente un'eccezione.
+            if (x.getCommand().getClass() == CommBaseArgs.class)
+            {
+                baseArgs = (CommBaseArgs) x.getCommand();
 
-				ArrayList<Field> baseFields = new ArrayList<Field>(
-						Arrays.asList(CommBaseArgs.class.getDeclaredFields()));
-				baseFields.remove(0);
-				baseFields.remove(baseFields.size() - 1);
+                ArrayList<Field> baseFields = new ArrayList<Field>(
+                        Arrays.asList(CommBaseArgs.class.getDeclaredFields()));
+                baseFields.remove(0);
+                baseFields.remove(baseFields.size() - 1);
 
-				for (Field y : baseFields) {
-					// System.out.println(y.getName());
-					y.setAccessible(true);
-					try {
-						if (y.get(baseArgs).toString().equals("true")) {
-							if (baseArgs.active)
-								throw new IllegalStateException(); // CATCHED
-							else
-								baseArgs.active = true;
-						}
+                for (Field y : baseFields)
+                {
+                    // System.out.println(y.getName());
+                    y.setAccessible(true);
+                    try
+                    {
+                        if (y.get(baseArgs).toString().equals("true"))
+                        {
+                            if (baseArgs.active)
+                                throw new IllegalStateException(); // CATCHED
+                            else
+                                baseArgs.active = true;
+                        }
 
-						y.setAccessible(false);
-					} catch (IllegalArgumentException e) {
-						e.printStackTrace();
-					} catch (IllegalAccessException e) {
-						e.printStackTrace();
-					}
-				}
-			}
+                        y.setAccessible(false);
+                    } catch (IllegalArgumentException e)
+                    {
+                        e.printStackTrace();
+                    } catch (IllegalAccessException e)
+                    {
+                        e.printStackTrace();
+                    }
+                }
+            }
 
-			// Il comando parsato � "-w"
-			else if (x.getCommand().getClass() == CommWorkspace.class) {
-				workspace = (CommWorkspace) x.getCommand();
-				workspace.active = true;
-			}
-		}
-	}
+            // Il comando parsato � "-w"
+            else if (x.getCommand().getClass() == CommWorkspace.class)
+            {
+                workspace = (CommWorkspace) x.getCommand();
+                workspace.active = true;
+            }
+        }
+    }
 
-	public CommBaseArgs getBaseArgs() {
-		return baseArgs;
-	}
+    public CommBaseArgs getBaseArgs()
+    {
+        return baseArgs;
+    }
 
-	public CommWorkspace getCommWorkspace() {
-		return workspace;
-	}
-	
-	//==========================================================
-	//================INTERFACE IMPLEMENTATION==================
-	//==========================================================
+    public CommWorkspace getCommWorkspace()
+    {
+        return workspace;
+    }
+
+    // ==========================================================
+    // ================INTERFACE IMPLEMENTATION==================
+    // ==========================================================
 
     @Override
     public Boolean help()
     {
-        // TODO Auto-generated method stub
-        return null;
+        if(baseArgs.getHelpStatus())
+            return true;
+        return false;
     }
 
     @Override
     public Boolean users()
     {
-        // TODO Auto-generated method stub
-        return null;
+        if(workspace.getMembersStatus())
+            return true;
+        return false;
     }
 
     @Override
     public Boolean channels()
     {
-        // TODO Auto-generated method stub
-        return null;
+        if(workspace.getChannelsStatus())
+            return true;
+        return false;
     }
 
     @Override
     public Boolean extendedChannels()
     {
-        // TODO Auto-generated method stub
-        return null;
+        if(workspace.getExtChannelsStatus())
+            return true;
+        return false;
     }
 
     @Override
@@ -245,5 +277,56 @@ public class CommandParser implements CommandParserInterface {
     {
         // TODO Auto-generated method stub
         return null;
+    }
+
+    // ==========================================================
+    // ====================INTERFACE HELPERS=====================
+    // ==========================================================
+
+    private Boolean wantsFrom(String[] mentionParams)
+    {
+        if ((mentionParams.length == 2) && mentionParams[0].equals("from")
+                && mentionParams[1] != null)
+            return true;
+        else
+            return false;
+    }
+
+    private Boolean wantsTo(String[] mentionParams)
+    {
+        if ((mentionParams.length == 2) && mentionParams[0].equals("to")
+                && mentionParams[1] != null)
+            return true;
+        else
+            return false;
+    }
+
+    private Boolean wantsIn(String[] mentionParams)
+    {
+        if ((mentionParams.length == 2) && mentionParams[0].equals("in")
+                && mentionParams[1] != null)
+            return true;
+        else
+            return false;
+    }
+
+    private Boolean wantsFromIn(String[] mentionParams)
+    {
+        if ((mentionParams.length == 4) && mentionParams[0].equals("from")
+                && mentionParams[1] != null && mentionParams[2].equals("in")
+                && mentionParams[1] != null)
+            return true;
+        else
+            return false;
+    }
+
+    private Boolean wantsToIn(String[] mentionParams)
+    {
+        if ((mentionParams.length == 4) && mentionParams[0].equals("to")
+                && mentionParams[1] != null && mentionParams[2].equals("in")
+                && mentionParams[1] != null)
+            return true;
+        else
+            return false;
     }
 }
