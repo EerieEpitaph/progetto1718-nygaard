04497d4747d12d1ac1677245899e67980e6be059
EerieEpitaph <o.lischio@studenti.uniba.it>
2018-05-21 13:53:27 +0200

    Message.java compliant

--- src/main/java/it/uniba/workdata/Message.java
+++ src/main/java/it/uniba/workdata/Message.java
@@ -1,52 +1,102 @@
 package it.uniba.workdata;
 
+/**
+ * This class wraps GsonMessage to better present it.
+ */
 public class Message {
-	public class GsonMessage {
-		private String type;
-		private String user;
-		private String text;
 
-		public GsonMessage(final String _type, final String _user, final String _text) {
-			type = _type;
-			user = _user;
-			text = _text;
+	/**
+	 * Gson message to wrap.
+	 */
+	private final GsonMessage toWrap;
+
+	/**
+	 * This class models a message (.json files inside folders).
+	 */
+	public static final class GsonMessage {
+
+		/**
+		 * Type of message.
+		 */
+		private final String type;
+
+		/**
+		 * User id that sent it.
+		 */
+		private final String user;
+
+		/**
+		 * Message text.
+		 */
+		private final String text;
+
+		/**
+		 * GsonMessage constructor.
+		 * @param type2 message type
+		 * @param user2 user id
+		 * @param text2 message text
+		 */
+		public GsonMessage(final String type2, final String user2, final String text2) {
+			type = type2;
+			user = user2;
+			text = text2;
 		}
 
+		/**
+		 * Type getter.
+		 * @return message type as String
+		 */
 		public String getType() {
 			return type;
 		}
 
+		/**
+		 * User getter.
+		 * @return user id as String
+		 */
 		public String getUser() {
 			return user;
 		}
 
+		/**
+		 * Text getter.
+		 * @return message text as String
+		 */
 		public String getText() {
 			return text;
 		}
 	}
 
-	// private String channel;
-	private GsonMessage message;
-
-	public Message(GsonMessage _message) {
+	/**
+	 * Message constructor.
+	 * @param message2 message to wrap
+	 */
+	public Message(final GsonMessage message2) {
 		// channel = _channel;
-		message = _message;
+		toWrap = message2;
 	}
 
-	// public String getChannel()
-	// {
-	// return channel;
-	// }
-
+	/**
+	 * Wrapper type returner.
+	 * @return GsonMessage type
+	 */
 	public String getType() {
-		return message.getType();
+		return toWrap.getType();
 	}
 
+	/**
+	 * Wrapper user returner.
+	 * @return GsonMessage user
+	 */
 	public String getUser() {
-		return message.getUser();
+		return toWrap.getUser();
 	}
 
+	/**
+	 * Wrapper text returner.
+	 * @return GsonMessage text
+	 */
 	public String getText() {
-		return message.getText();
+		return toWrap.getText();
 	}
 }
