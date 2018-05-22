40e3934c0569856009201520bebb401724f2d1b0
Giovanni Dimeo <giovandimeo@gmail.com>
2018-05-22 17:44:02 +0200

    JavaDoc sul Model

--- src/main/java/it/uniba/model/Model.java
+++ src/main/java/it/uniba/model/Model.java
@@ -10,17 +10,44 @@ import it.uniba.workdata.Channel;
 import it.uniba.workdata.Message;
 import it.uniba.workdata.User;
 
+/**
+ * <i>Model</i> storage and manipulate Data
+ */
+
 public class Model {
 	// WorkData
+	/**
+	 * Map of <i>Users</i>
+	 */
 	private HashMap<String, User> users = new HashMap<String, User>();
+	/**
+	 * Map of <i>Channels</i>
+	 */
 	private HashMap<String, Channel> channels = new HashMap<String, Channel>();
+	/**
+	 * Map of <i>Messages</i> group by channels
+	 */
 	private HashMap<String, ArrayList<Message>> messages = new HashMap<String, ArrayList<Message>>();
 
-	// MentionGraph
+	/**
+	 * Graph of all mentions present in the workspace
+	 */
 	MentionGraph snagraph = new MentionGraph();
 
+	/**
+	 * ZipParser for loading data from zip file
+	 */
 	ZipParser fileParser = new ZipParser();
 
+	/**
+	 * Load all data of Model from procedure load of class ZipParser
+	 * 
+	 * @param path
+	 *            <i>String</i> Path of Zipfile
+	 * @throws ZipException
+	 *             Exception of ZipParser
+	 * @throws IOException
+	 */
 	public void updateModel(String path) throws ZipException, IOException {
 		fileParser.load(path);
 
@@ -29,32 +56,66 @@ public class Model {
 		messages = fileParser.getMessages();
 	}
 
+	/**
+	 * Check if fileParser has load data from zip file
+	 * 
+	 * @return <i>boolean</i> if fileParser has load data from zip file
+	 */
 	public boolean hasLoaded() {
 		return fileParser.hasLoaded();
 	}
 
+	/**
+	 * Default costructor of Model
+	 */
 	public Model() {
 	}
 
-	public Model(HashMap<String, User> _users, HashMap<String, Channel> _channels,
-			HashMap<String, ArrayList<Message>> _messages) {
-		users = _users;
-		channels = _channels;
-		messages = _messages;
-	}
+	// /**
+	// * Initialize a generic model after load the data
+	// * @param _users <i>HashMap</i> of all <b>user</b> present in the workspace
+	// * @param _channels <i>HashMap</i> of all <b>channels</b> present in the
+	// workspace
+	// * @param _messages <i>HashMap</i> of all <b>Messages</b> present in the
+	// workspace
+	// */
+	// public Model(HashMap<String, User> _users, HashMap<String, Channel>
+	// _channels,
+	// HashMap<String, ArrayList<Message>> _messages) {
+	// users = _users;
+	// channels = _channels;
+	// messages = _messages;
+	// }
 
+	/**
+	 * 
+	 * @return <i>HashMap</i> of Users
+	 */
 	public HashMap<String, User> getUsers() {
 		return users;
 	}
 
+	/**
+	 * 
+	 * @return <i>HashMap</i> of all Channels
+	 */
 	public HashMap<String, Channel> getChannels() {
 		return channels;
 	}
 
+	/**
+	 * 
+	 * @return <i>HashMap</i> of all messagges
+	 */
 	public HashMap<String, ArrayList<Message>> getMessages() {
 		return messages;
 	}
 
+	/**
+	 * 
+	 * @return <i>MentionGraph</i> an instance of MentionGraph witch contains all
+	 *         mentions
+	 */
 	public MentionGraph getMentionGraph() {
 		return snagraph;
 	}
