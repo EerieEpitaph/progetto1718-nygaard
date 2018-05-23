b4c55a92c43a3f794cdf2ec45996560ee061e6ff
Nicola Curci <37325999+NicolaSigSky@users.noreply.github.com>
2018-05-23 21:59:35 +0200

    [Closes] Test automatici nel package Model (#74)
    * Aggiunto Test su edgesOutDegree
    
    * Fix con il reset sul MentionGraphTest
    
    * Test su OutDegree e inDegree in MentionGraphTest

---
+++ src/test/java/it/uniba/model/MentionGraphTest.java
@@ -0,0 +1,61 @@
+package it.uniba.model;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.zip.ZipException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import it.uniba.model.Edge;
+import it.uniba.model.Model;
+
+import it.uniba.workdata.User;
+
+public class MentionGraphTest {
+ static ArrayList<ArrayList<Edge>> edges = new ArrayList<ArrayList<Edge>>();
+ static ArrayList<Edge> ed = new ArrayList<Edge>();
+ 
+ static Model mod = new Model();
+
+ static User u;
+ static User v;
+
+ @BeforeAll
+ static void Init() throws ZipException, IOException {
+
+   mod.updateModel(".//res//ingsw.zip");
+   mod.getMentionGraph().parseMessages(mod.getMessages(), mod.getUsers(), "");
+   
+   u = mod.getUsers().get("U9P18U17X");  // Manlio Amato
+   v = mod.getUsers().get("U9NF6NSU8");
+   ed.add(new Edge(u, v, 2));
+   v = mod.getUsers().get("U9NBNJFB3");
+   ed.add(new Edge(u,v,2));
+   edges.add(ed);
+   // to Andrea Di Fonzo
+  // ed.clear();
+   ed = new ArrayList<Edge>();
+   u = mod.getUsers().get("U9NCNLL83"); // Serena DeRuvo 
+   v = mod.getUsers().get("U9NAWRB2Q"); // Andrea DiFonzo
+
+   ed.add(new Edge(u,v,1));
+   edges.add(ed);
+ }
+
+ @Test
+ void edgesOutDegreeTest() throws ZipException, IOException {
+	u = mod.getUsers().get("U9P18U17X");
+	assertEquals(edges.get(0), mod.getMentionGraph().edgesOutDegree(u));
+ }
+ 
+ @Test
+ void edgesInDegreeTest() {
+	 v = mod.getUsers().get("U9NAWRB2Q");
+	 assertEquals(edges.get(1), mod.getMentionGraph().edgesInDegree(v));
+ }
+}
