c5a3c7da154e8b497f6d485d3a4dfb3f55a807b8
Nicola Curci <curcinicola05@gmail.com>
2018-05-30 17:14:05 +0200

    Aggiunto assert membri

--- src/test/java/it/uniba/workdata/ChannelTester.java
+++ src/test/java/it/uniba/workdata/ChannelTester.java
@@ -10,25 +10,25 @@ import org.junit.jupiter.api.Test;
 
 public class ChannelTester {
 	static Channel chan;
-	static List<String> members;
+	static ArrayList<String> members;
 
 	@BeforeAll
-	static void setUpAll()
-	{
+	static void setUpAll() {
 		members = new ArrayList<String>();
 		members.add("Ottavio");
 		members.add("Giovanni");
 		members.add("Nicola");
 		members.add("Valentina");
 		// per il test l'id è una stringa personalizzata
-	    	chan = new Channel("#idNygaard",2018L,"phonixCreatore",members,"Giovanni");
+		chan = new Channel("#idNygaard", 2018L, "phonixCreatore", members, "Giovanni");
 	}
 
 	@Test
 	void channelGetterTest() {
-			assertAll("Check Channel data", () -> {
-			assertEquals("#idNygaard",chan.getId());
-			assertEquals(new Long(2018L),chan.getDateCreation());
+		assertAll("Check Channel data", () -> {
+			assertEquals("#idNygaard", chan.getId());
+			assertEquals(members, chan.getMemberList());
+			assertEquals(new Long(2018L), chan.getDateCreation());
 			assertEquals("phonixCreatore", chan.getCreator());
 			assertEquals("Giovanni", chan.getName());
 		});
