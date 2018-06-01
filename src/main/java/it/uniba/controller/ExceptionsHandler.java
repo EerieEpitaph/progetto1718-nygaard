22bf6b3ba748af874291ff6e2fbcab0df1a4602a
Giovanni Dimeo <giovandimeo@gmail.com>
2018-06-01 10:38:39 +0200

    Aggiungo JavaDoc su ExceptionsHandler

--- src/main/java/it/uniba/controller/ExceptionsHandler.java
+++ src/main/java/it/uniba/controller/ExceptionsHandler.java
@@ -1,11 +1,23 @@
 package it.uniba.controller;
 
 @SuppressWarnings("serial") // Da applicare il pattern visto a lezione
+/**
+ * 
+ * Custom Exceptions for sna4Slack 
+ *
+ */
 public class ExceptionsHandler extends Exception {
+	/**
+	 * Constructor of ExceptionsHandler 
+	 */
 	public ExceptionsHandler() {
 		// This constructor is intentionally empty. Nothing special is needed here.
 	}
-
+	/**
+	 * 
+	 * @param message
+	 * 				<i>String</i> Exception's message
+	 */
 	public ExceptionsHandler(String message) {
 		super(message);
 	}
